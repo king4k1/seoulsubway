2368c4b4e3c8349f551bb9526a46a28a27631e70
Son <kinh4k@gmail.com>
2018-11-28 18:25:16 +0900

    get default.

--- R/defaultset.R
+++ R/defaultset.R
@@ -1,6 +1,4 @@
-depart <- iconv("먹골",from="cp949",to="euc-KR")
-depart <- iconv(depart,from="euc-KR",to="utf8")
+depart <- "먹골"
 depart_line <- "7"
-arrival <- iconv("태릉입구",from="cp949",to="euc-KR")
-arrival <- iconv(arrival,from="euc-KR",to="utf8")
+arrival <- "태릉입구"
 arrival_line <- "4"
