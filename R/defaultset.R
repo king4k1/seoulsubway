1dc5dcbdd75f35a7ad35ec87111608c51db7ea38
Son <kinh4k@gmail.com>
2018-11-28 18:22:26 +0900

    input default set and change depend packages.

---
+++ R/defaultset.R
@@ -0,0 +1,6 @@
+depart <- iconv("먹골",from="cp949",to="euc-KR")
+depart <- iconv(depart,from="euc-KR",to="utf8")
+depart_line <- "7"
+arrival <- iconv("태릉입구",from="cp949",to="euc-KR")
+arrival <- iconv(arrival,from="euc-KR",to="utf8")
+arrival_line <- "4"
