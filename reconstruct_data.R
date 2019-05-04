38a8368dc633dd1347d4a509bbe5c65224ca67f8
KM Son <kinh4k@gmail.com>
2019-05-04 17:52:15 +0900

    it is useful for matching string or line with seoulsubway when usin subway data

--- reconstruct_data.R
+++ reconstruct_data.R
@@ -5,56 +5,50 @@
 
 reconstruct_data <- function(raw_data, colname_list=c("depart","depart_line",
                                                   "arrival","arrival_line")){
-  up_Line <- raw_data %>% dplyr::select(colname_list[2]) %>% t() %>% as.vector()
-  down_Line <- raw_data %>% dplyr::select(colname_list[4]) %>% t() %>% as.vector()
-  up_Name <- raw_data %>% dplyr::select(colname_list[1]) %>% t() %>% as.vector()
-  up_Name_index <- which(str_detect(up_Name, "[(]"))
-  up_Name_index_nm <- str_locate(up_Name[up_Name_index], "[(]")
-  up_Name[up_Name_index] <- substr(up_Name[up_Name_index], 1, 
-           up_Name_index_nm[, 1] - 1)
-  down_Name <- raw_data %>% dplyr::select(colname_list[3]) %>% t() %>% as.vector()
-  down_Name_index <- which(str_detect(down_Name, "[(]"))
-  down_Name_index_nm <- str_locate(down_Name[down_Name_index], "[(]")
-  down_Name[down_Name_index] <- substr(down_Name[down_Name_index], 1, 
-           down_Name_index_nm[, 1] - 1)  
-  down_Name <- str_replace(down_Name, "서울역", "서울")
-  up_Name <- str_replace(up_Name, "서울역", "서울")
-  down_Name <- str_replace(down_Name, "이수", "총신대입구")
-  up_Name <- str_replace(up_Name, "이수", "총신대입구")
+  dep_line <- raw_data %>% dplyr::select(colname_list[2]) %>% t() %>% as.character()
+  arr_line <- raw_data %>% dplyr::select(colname_list[4]) %>% t() %>% as.character()
+  dep_name <- raw_data %>% dplyr::select(colname_list[1]) %>% t() %>% as.character()
+  dep_name_index <- which(str_detect(dep_name, "[(]"))
+  dep_name_index_nm <- str_locate(dep_name[dep_name_index], "[(]")
+  dep_name[dep_name_index] <- str_sub(dep_name[dep_name_index], 1, 
+           dep_name_index_nm[, 1] - 1)
+  arr_name <- raw_data %>% dplyr::select(colname_list[3]) %>% t() %>% as.character()
+  arr_name_index <- which(str_detect(arr_name, "[(]"))
+  arr_name_index_nm <- str_locate(arr_name[arr_name_index], "[(]")
+  arr_name[arr_name_index] <- str_sub(arr_name[arr_name_index], 1, 
+           arr_name_index_nm[, 1] - 1)  
+  arr_name <- str_replace(arr_name, "서울역", "서울")
+  dep_name <- str_replace(dep_name, "서울역", "서울")
+  arr_name <- str_replace(arr_name, "이수", "총신대입구")
+  dep_name <- str_replace(dep_name, "이수", "총신대입구")
   
-  up_Line <- str_sub(up_Line, 1, 1) # ex. "1호선" -> "1"
-  down_Line <- str_sub(down_Line, 1, 1) # ex. "1호선" -> "1"
-
+  dep_line <- str_sub(dep_line, 1, 1) # ex. "1호선" -> "1"
+  arr_line <- str_sub(arr_line, 1, 1) # ex. "1호선" -> "1"
   ind_2_A_up <- 
-    which(up_Name%in%c("용답", "신답", "용두","신설동")& 
-            up_Line%in%c("2"))
+    which(dep_name%in%c("용답", "신답", "용두","신설동")& dep_line%in%c("2"))
   ind_2_B_up <- 
-    which(up_Name%in%c("도림천", "양천구청", "신정네거리")& 
-            up_Line%in%c("2"))
+    which(dep_name%in%c("도림천", "양천구청", "신정네거리")& dep_line%in%c("2"))
   ind_5_A_up <- 
-    which(up_Name%in%c("둔촌동","올림픽공원", "방이","개롱","거여","마천","오금")& 
-            up_Line%in%c("5"))
+    which(dep_name%in%c("둔촌동","올림픽공원", "방이","개롱","거여","마천","오금")& 
+            dep_line%in%c("5"))
   ind_6_A_up <- 
-    which(up_Name%in%c("연신내", "불광", "역촌", "구산", "독바위", "응암")& 
-            up_Line%in%c("6"))
+    which(dep_name%in%c("연신내", "불광", "역촌", "구산", "독바위", "응암")& 
+            dep_line%in%c("6"))
 
   ind_2_A_down <- 
-    which(down_Name%in%c("용답", "신답", "용두","신설동")& 
-            down_Line%in%c("2"))
+    which(arr_name%in%c("용답", "신답", "용두","신설동")& arr_line%in%c("2"))
   ind_2_B_down <- 
-    which(down_Name%in%c("도림천", "양천구청", "신정네거리")& 
-            down_Line%in%c("2"))
+    which(arr_name%in%c("도림천", "양천구청", "신정네거리")& arr_line%in%c("2"))
   ind_5_A_down <- 
-    which(down_Name%in%c("둔촌동","올림픽공원", "방이","개롱","거여","마천","오금")& 
-            down_Line%in%c("5"))
+    which(arr_name%in%c("둔촌동","올림픽공원", "방이","개롱","거여","마천","오금")& 
+            arr_line%in%c("5"))
   ind_6_A_down <- 
-    which(down_Name%in%c("연신내", "불광", "역촌", "구산", "독바위", "응암")& 
-            down_Line%in%c("6"))
-  raw_data[,6] <- up_Name
-  raw_data[,4] <- up_Line
-  raw_data[,9] <- down_Name
-  raw_data[,7] <- down_Line
-    
+    which(arr_name%in%c("연신내", "불광", "역촌", "구산", "독바위", "응암")& 
+            arr_line%in%c("6"))
+  raw_data[,6] <- dep_name
+  raw_data[,4] <- dep_line
+  raw_data[,9] <- arr_name
+  raw_data[,7] <- arr_lin
   raw_data[ind_2_A_up,4] <- "2_A"
   raw_data[ind_2_B_up,4] <- "2_B"
   raw_data[ind_5_A_up,4] <- "5_A"
@@ -63,6 +57,5 @@ reconstruct_data <- function(raw_data, colname_list=c("depart","depart_line",
   raw_data[ind_2_B_down,7] <- "2_B"
   raw_data[ind_5_A_down,7] <- "5_A"
   raw_data[ind_6_A_down,7] <- "6_A"
-
   return(raw_data)
 }
