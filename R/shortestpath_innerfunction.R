9689b30b1bcc2c87dd579401821adb72b1da9ae2
KM Son <kinh4k@gmail.com>
2019-05-22 07:20:09 +0900

    update/

--- R/shortestpath_innerfunction.R
+++ R/shortestpath_innerfunction.R
@@ -44,13 +44,13 @@ get_pathinfo <- function(total, start, end, line) {
     Circulate <- total - abs(start - end)
     Circulate2 <- abs(start - end)
     Circulate <- c(Circulate, Circulate2)
-    Circulate_Time <- c(sum(subway_data[[line]][c(start:total,1:end),"Time", with=FALSE]) - 
-                          subway_data[[line]][start, "Time", with=FALSE],
-                        sum(subway_data[[line]][c(end:total,1:start),"Time", with=FALSE]) -
-                          subway_data[[line]][start,"Time", with=FALSE])
+    Circulate_Time <- c(sum(subway_data[[line]][c(start:total,1:end),]$Time) - 
+                          subway_data[[line]][start,]$Time,
+                        sum(subway_data[[line]][c(end:total,1:start),]$Time) -
+                          subway_data[[line]][start,]$Time)
     Circulate_Time <- Circulate_Time[which.min(Circulate_Time)]
-    Circulate2_Time <- sum(subway_data[[line]][start:end,"Time", with=FALSE]) - 
-      subway_data[[line]][start,"Time", with=FALSE]
+    Circulate2_Time <- sum(subway_data[[line]][start:end,]$Time) - 
+      subway_data[[line]][start,]$Time
     Circulate_Time <- c(Circulate_Time, Circulate2_Time)
     Path_Count <- Circulate[which.min(Circulate_Time)]
     Path_Time <- Circulate_Time[which.min(Circulate_Time)]
@@ -58,8 +58,8 @@ get_pathinfo <- function(total, start, end, line) {
   }
   if (line == "6_A" & start > end) {
     Path_Count <- 6 + end - start 
-    Path_Time <- sum(subway_data[[line]][c(end:6,1:start),"Time", with=FALSE]) - 
-      subway_data[[line]][start,"Time", with=FALSE] + 3 
+    Path_Time <- sum(subway_data[[line]][c(end:6,1:start),]$Time) - 
+      subway_data[[line]][start,]$Time + 3 
     # 6-A -> 6-A circulate line.
   }
   return(data.frame(count = as.numeric(Path_Count), time = as.numeric(Path_Time) %>% round(2)))
